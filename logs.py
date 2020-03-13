5fd70d83aa7cd61792ab37750a3f8b3e6eb624c9
dastangoo <hamed.dastangoo@gmail.com>
2020-03-13 12:59:45 +0330

    Add some examples

---
+++ logs.py
@@ -0,0 +1,7 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+print(username + " " + "accessed the site " + url + " at " + timestamp + ".")
+print("%s accessed the site %s at %s." % (username, url, timestamp))
+print(f"{username} accessed the site {url} at {timestamp}.")
+print("{} accessed the site {} at {}.".format(username, url, timestamp))
